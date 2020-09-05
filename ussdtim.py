f6b3e945cd4f48b5971fd7259bc75e47ea49188a
ajayitomisin1728@gmail.com <ajayitomisin1728@gmail.com>
2020-09-05 15:38:03 +0100

    added new fuctions and more functionality

--- ussdtim.py
+++ ussdtim.py
@@ -17,6 +17,9 @@ Select the Bank you wish to Transfer to:
 
 	
 def open_acct( ):
+	print("Welcome to our online Account opening services.")
+	print("loading...")
+# creating an empty list to serve as a temporary place holder.
 	temp_storage= [ ]
 	f_name= input("Enter your first name:")
 	s_name= input ("Enter your second name:")
@@ -25,40 +28,78 @@ def open_acct( ):
 	temp_storage.append(s_name)
 	temp_storage.append(sex)
 	print(temp_storage)
+	exit( )
 	
 def upgrade_migrate( ):
 		print("Welcome to our online Upgrade/Migration services.\n 1. Ugrade\n 2. Migrate")
-		prompt = input("Enter preferred Choice:")
+		prompt = int(input("Enter preferred Choice:"))
 		if prompt==1:
 			time.sleep(5)
-			print("Choose Upgrade choice.")
-		else:
+			print("Upgrading...")
+			exit( )
+		elif prompt == 2:
 			time.sleep(5)
-			print("Choose Migration Choice:")
+			print("Migrating...")
+			exit( )
+		elif prompt == "#":
+			options_menu( )
+		else:
+			sys.exit( )
 
 def balance ( ):
-	pass				
+	print("ACCOUNT\tBALANCE\n CHECKER")
+	print("press 0 is go back to the Main Menu.")
+	pin=input("Enter your 4 digit pin:")
+# isdigit( ) is used to  check for digits within a str
+	if pin.isdigit( )==True:
+		time.sleep(5)
+		print("Loading...")
+		exit( )
+# Tried using zero ,but it didnt work because zero is a digit wirhin a string 
+	elif pin== "#":
+		options_menu( )
+	else:
+		time.sleep(15)
+		print("wrong pin")
+		sys.exit( )
+		
+def transf( ):
+	pass
+	
+def recharge( ):
+	pass													
 
-def options ( ) :
-	print(" 1. Open Account\n2. Upgrade/Migrate\n3. Balance\n4. Transfer\n5. Recharge")
+def options_menu( ) :
+	print("1. Open Account\n2. Upgrade/Migrate\n3. Balance\n4. Transfer\n5. Recharge")
 	choice=int(input("Enter an option:"))
 	if choice == 1:
-		print("Welcome to our online Account opening services.")
-		print("loading...")
 		time.sleep(10)
 		open_acct( )
 	if choice == 2:
 		time.sleep(10)
 		upgrade_migrate( )
-	if choice ==3:
+	elif choice ==3:
 		time.sleep(10)
 		balance( )
-		
-		
-		
-	
+	elif choice ==4:
+		time.sleep(10)
+		transf( )
+	elif choice ==5:
+		time.sleep( )
+		recharge( )
+	else:
+		time.sleep(5)
+		print("Not an option.")
+		sys.exit( )
 
-def main( ):
+def exit( ):
+	exit= input("Do you wish to make another transaction [Y/N] :")
+	if exit== "N":
+		sys.exit( )
+	else:
+		log_in( )
+				
+def log_in( ):
     try:
         a=0
         while a<3:
@@ -68,18 +109,18 @@ def main( ):
             	 print("please re-enter USSD ...")
             else:
             	print("Welcome to our online services how may we help you")
-            	options( )
-            	sys.exit( )
+            	options_menu( ) 
+            	exit( )
         else:
-        	sys.exit( )
+        	time.sleep(10)
+        	print("checking discrepancies...")
+        	time.sleep(5)
+        	print("An error has occured.")
     
     except:
-    	time.sleep(10)
-    	print("checking discrepancies...")
-    	time.sleep( 5)
-    	print("An error has occured.")	
-
-main( )
+    		sys.exit( )
+    		
+log_in( )
 
 	
 
